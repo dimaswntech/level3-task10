a2bfc37f4c49d275d9ebc69ad52453a7be52a186
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-09-14 01:21:31 +0700

    setting for update

--- components/home/tableProduct.controller.ts
+++ components/home/tableProduct.controller.ts
@@ -2,27 +2,29 @@ import Vue from "vue";
 import Component from "nuxt-class-component";
 
 @Component({
-data(){
-  return{
-    dataTheads:["No.", "Nama Produk", "Keterangan Produk", "Harga Produk", "Jumlah Produk"],
-    dataProducts: [],
+  data() {
+    return {
+      dataTheads: ["No.", "Nama Produk", "Keterangan Produk", "Harga Produk", "Jumlah Produk"],
+      dataProducts: [],
+      showModal: false,
+      idVal: {}
+    }
   }
-}
 })
-export default class tableProduct extends Vue{
-async mounted() {
-  //fething ke Rest API
-  this.$axios.get('/api/products')
-    .then(response => {
+export default class tableProduct extends Vue {
+  async mounted() {
+    //fething ke Rest API
+    this.$axios.get('/api/products')
+      .then(response => {
 
-      //assign response ke state "posts"
-      this.$data.dataProducts = response.data.data
+        //assign response ke state "posts"
+        this.$data.dataProducts = response.data.data
 
-    })
-    .catch(error => {
-      console.log(error.response.data)
-    })
-}
+      })
+      .catch(error => {
+        console.log(error.response.data)
+      })
+  }
 
   async deletePost(row: any) {
 
@@ -34,6 +36,18 @@ async mounted() {
         this.$data.dataProducts.splice(row.index, 1);
         window.location.reload();
       })
+  }
 
+  toggleModal(id: any) {
+    this.$data.idVal = id;
+    this.$data.showModal = !this.$data.showModal;
+  }
+
+  valueShow(val: any) {
+    this.$data.showModal = val;
+  }
+
+  refreshPage() {
+    window.location.reload();
   }
 }
--- components/home/tableProduct.vue
+++ components/home/tableProduct.vue
@@ -1,28 +1,32 @@
 <template>
-  <table>
-    <thead>
-    <tr>
-      <td>No</td>
-      <td>Nama Product</td>
-      <td>Keterangan Product</td>
-      <td>Harga Product</td>
-      <td>Jumlah Product</td>
-      <td>Actions</td>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="(product, index) in dataProducts" :key="index">
-      <td>{{ index + 1 }}</td>
-      <td>{{ product.nama_produk }}</td>
-      <td>{{ product.keterangan }}</td>
-      <td>{{ product.harga }}</td>
-      <td>{{ product.jumlah }}</td>
-      <td>
-        <nuxt-link :to="{name: 'update-id', params:{id:product.id}}">input</nuxt-link>
-        || <p @click="deletePost(product.id)">Delete</p></td>
-    </tr>
-    </tbody>
-  </table>
+  <div>
+    <table class="text-center table-auto opacity-75 shadow-lg">
+      <thead class="border-b bg-gray-50">
+      <tr>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">No</th>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">Nama Product</th>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">Keterangan Product</th>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">Harga Product</th>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">Jumlah Product</th>
+        <th class="text-sm font-medium text-gray-900 px-6 py-4">Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(product, index) in dataProducts" :key="index" class="bg-white border-b">
+        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ index + 1 }}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ product.nama_produk }}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ product.keterangan }}</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">Rp. {{ product.harga }},00</td>
+        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ product.jumlah }}</td>
+        <td class="flex text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+          <p @click="toggleModal(product)" class="py-1 px-5 bg-yellow-100 mr-3 active:shadow-lg hover:bg-yellow-300 cursor-pointer">EDIT</p>
+          <p @click="deletePost(product.id)" class="py-1 px-5 bg-red-100 active:shadow-lg hover:bg-red-300 cursor-pointer">DELETE</p>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <modal-editProduct :show="showModal" :product="idVal" @customModal="valueShow" @successAdd="refreshPage"></modal-editProduct>
+  </div>
 </template>
 <script lang="ts">
 export {default} from './tableProduct.controller';
