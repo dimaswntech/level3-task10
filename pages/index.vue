4810cc8a8e5821239efe3f34922e0c7d468b127b
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-09-13 22:46:08 +0700

    add delete method

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,74 @@
 <template>
-  <div>
-    <table>
-      <thead>
-      <tr>
-        <td>No</td>
-        <td>Nama Product</td>
-        <td>Keterangan Product</td>
-        <td>Harga Product</td>
-        <td>Jumlah Product</td>
-        <td>Actions</td>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(post, index) in posts" :key="index">
-        <td>{{ index+1 }}</td>
-        <td>{{ post.nama_produk }}</td>
-        <td>{{ post.keterangan }}</td>
-        <td>{{ post.harga }}</td>
-        <td>{{ post.jumlah }}</td>
-        <td><nuxt-link :to="{name: 'update-id', params:{id:post.id}}">input</nuxt-link> || <p @click="deletePost(post.id)">Delete</p></td>
-      </tr>
-      </tbody>
-    </table>
+  <div class=" flex flex-col items-center justify-center">
+    <p>Data Gudang</p>
+    <p>Toko Kelontong Pijar</p>
+    <button @click="toggleModal">Tambah Data</button>
+    <home-table-product></home-table-product>
 
+    <modal-tambahProduct :show="showModal" @customModal="valueShow" @successAdd="refreshPage"></modal-tambahProduct>
   </div>
 </template>
 
 <script>
 export default {
-
   data() {
     return {
-      posts: [],
+      showModal: false
     }
-  },
-
-  mounted() {
-
-    //fething ke Rest API
-    this.$axios.get('/api/products')
-      .then(response => {
-
-        //assign response ke state "posts"
-        this.posts = response.data.data
-
-      })
-      .catch(error => {
-        console.log(error.response.data)
-      })
   },
   methods: {
-
-    async deletePost(row) {
-      await this.$axios.delete(`/api/products/${row}`)
-        .then(() => {
-
-          //remove item array by index
-          this.posts.splice(row.index, 1);
-
-        })
-
+    toggleModal: function(){
+      this.showModal = !this.showModal;
+    },
+    valueShow: function (val) {
+      this.showModal = val;
+    },
+    refreshPage: function () {
+      window.location.reload();
     }
   }
-
 }
 </script>
+
+<!--<script>-->
+<!--export default {-->
+
+<!--  data() {-->
+<!--    return {-->
+<!--      posts: [],-->
+<!--    }-->
+<!--  },-->
+
+<!--  mounted() {-->
+
+<!--    //fething ke Rest API-->
+<!--    this.$axios.get('/api/products')-->
+<!--      .then(response => {-->
+
+<!--        //assign response ke state "posts"-->
+<!--        this.posts = response.data.data-->
+
+<!--      })-->
+<!--      .catch(error => {-->
+<!--        console.log(error.response.data)-->
+<!--      })-->
+<!--  },-->
+<!--  methods: {-->
+
+<!--    async deletePost(row) {-->
+<!--      await this.$axios.delete(`/api/products/${row}`)-->
+<!--        .then(() => {-->
+
+<!--          //remove item array by index-->
+<!--          this.posts.splice(row.index, 1);-->
+
+<!--        })-->
+
+<!--    }-->
+<!--  }-->
+
+<!--}-->
+<!--</script>-->
 
 <style>
 
