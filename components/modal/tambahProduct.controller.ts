1dea092f5fdd3beb24687f86d71ad44c7fd0b46c
Dimas Wahyu Notonagoro <dimaswntech@gmail.com>
2022-09-13 22:45:38 +0700

    modal add product

---
+++ components/modal/tambahProduct.controller.ts
@@ -0,0 +1,56 @@
+import Vue from "vue";
+import Component from "nuxt-class-component";
+
+@Component({
+  props: {
+    show: Boolean
+  },
+  data() {
+    return {
+      products: {
+        nama_produk: '',
+        keterangan: '',
+        harga: null,
+        jumlah: null
+      },
+      //state validation
+      validation: []
+    }
+  }
+})
+
+export default class tambahProduct extends Vue {
+  toggleModal() {
+    this.$emit("customModal")
+  }
+  //method "store"
+  async store() {
+
+    //send data ke Rest API
+    await this.$axios.post('/api/products', {
+
+      //data yang dikirim ke server
+      nama_produk: this.$data.products.nama_produk,
+      keterangan: this.$data.products.keterangan,
+      harga: this.$data.products.harga,
+      jumlah: this.$data.products.jumlah,
+
+    })
+      .then(() => {
+
+        //redirect ke route "post"
+        // this.$router.push({
+        //   name: 'home'
+        // })
+        this.$emit("customModal")
+        this.$emit("successAdd")
+
+      })
+      .catch(error => {
+        //assign validation
+        this.$data.validation = error.response.data
+      })
+  }
+
+
+}
